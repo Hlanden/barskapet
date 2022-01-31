3ccb24a7a50e5a509834005980525c8f23ede335
Jørgen Haaland <jorghaal@stud.ntnu.no>
2022-01-31 17:21:31 +0100

    Add max wait limit in tts module

--- python/src/tts.py
+++ python/src/tts.py
@@ -13,8 +13,14 @@ def speak(sentence, wait_on_mode_tts=False):
     if not wait_on_mode_tts:
         os.system('pkill -9 espeak')
     else:
-        while "espeak" in (p.name() for p in psutil.process_iter()):
+        i = 0
+        while i < 200 and "espeak" in (p.name() for p in psutil.process_iter()):
             time.sleep(0.1)
+            i += 1
+        # something is wrong, kill all espeak processes
+        if i == 200:
+            os.system('pkill -9 espeak')
+
     os.system('espeak -s 150 -a 200 "{}"'.format(sentence))
 
 def change_mode(mode, wait_on_mode_tts=False):
